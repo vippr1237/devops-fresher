2fff51e08a0b70537ce2ed15b0d58c5578f73b23
vippr1237 <vippr1239@gmail.com>
2021-07-20 10:44:03 +0700

    oauth2-resource-sever

--- src/test/java/com/ifisolution/bussiness_management/SpringIntegrationTest.java
+++ src/test/java/com/ifisolution/bussiness_management/SpringIntegrationTest.java
@@ -1,13 +1,13 @@
 package com.ifisolution.bussiness_management;
 
 import io.cucumber.spring.CucumberContextConfiguration;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@Log4j2
+@ContextConfiguration
 @CucumberContextConfiguration
 @SpringBootTest(classes = {BussinessManagementApplication.class, CucumberIntegrationTest.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 //cần tìm hiểu thêm
-//class này để config stepdefs
+//class này để config
 public class SpringIntegrationTest {
 }
